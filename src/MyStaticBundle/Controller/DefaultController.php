53d4557675f465ae19dff96f5eaaab86e3b722ab
KevinCarnaille <kevin@ylly.fr>
2015-09-19 17:17:32 +0200

    SEO Sonata Bundle First Implementation

--- src/MyStaticBundle/Controller/DefaultController.php
+++ src/MyStaticBundle/Controller/DefaultController.php
@@ -8,10 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $pagename = 'Homepage';
 
-        return $this->render('MyStaticBundle:Default:index.html.twig', array(
-            'pagename' => $pagename
-        ));
+        //SEO Sonata override//
+        $seoPage = $this->container->get('sonata.seo.page');
+        $seoPage
+            ->setTitle("Homepage - Front-End Skeleton")
+            ->addMeta('name', 'keywords', "Front-end, symfony2")
+            ->addMeta('name', 'description', "Front-End Skeleton demo for new projects")
+            ->addMeta('property', 'og:title', "Homepage - Front-End Skeleton")
+            ->addMeta('property', 'og:type', 'blog')
+            ->addMeta('property', 'og:url',  $this->generateUrl('homepage',array(),true))
+            ->addMeta('property', 'og:description', "Front-End Skeleton demo for new projects")
+        ;
+        // END //
+        return $this->render('MyStaticBundle:Default:index.html.twig');
     }
 }
